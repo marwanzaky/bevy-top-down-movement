7dfc95aacbfb3306af4e18e119005bb9020db38c
Marwan Zaky <marouaneezzaky@gmail.com>
2023-10-14 18:42:08 +0100

    Refactor the code

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,72 @@
 use bevy::prelude::*;
 
-#[derive(Component)]
-enum Direction {
-    Up,
-    Down,
+#[derive(Resource, Default)]
+struct Game {
+    player: Player,
+}
+
+#[derive(Default)]
+struct Player {
+    entity: Option<Entity>,
+    translation: Vec3,
 }
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .init_resource::<Game>()
         .add_systems(Startup, setup)
         .add_systems(Update, sprite_movement)
         .run();
 }
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>, mut game: ResMut<Game>) {
+    game.player.translation = Vec3::new(0., 0., 0.);
+
     commands.spawn(Camera2dBundle::default());
 
-    commands.spawn((
-        SpriteBundle {
-            texture: asset_server.load("icon.png"),
-            transform: Transform::from_xyz(0., 0., 0.),
-            ..default()
-        },
-        Direction::Up,
-    ));
+    game.player.entity = Some(
+        commands
+            .spawn(SpriteBundle {
+                texture: asset_server.load("icon.png"),
+                transform: Transform::from_xyz(0., 0., 0.),
+                ..default()
+            })
+            .id(),
+    );
 }
 
 fn sprite_movement(
     keyboard_input: Res<Input<KeyCode>>,
     time: Res<Time>,
-    mut sprite_position: Query<(&mut Direction, &mut Transform)>,
+    mut game: ResMut<Game>,
+    mut transforms: Query<&mut Transform>,
 ) {
-    for (mut direction, mut transform) in &mut sprite_position {
-        let mut movement: Vec3 = Vec3::new(0., 0., 0.);
-        let speed: f32 = 150.;
+    let mut movement: Vec3 = Vec3::new(0., 0., 0.);
+    let speed: f32 = 150.;
 
-        if keyboard_input.pressed(KeyCode::Right) {
-            movement.x += 1.;
-        }
-
-        if keyboard_input.pressed(KeyCode::Left) {
-            movement.x -= 1.;
-        }
-
-        if keyboard_input.pressed(KeyCode::Up) {
-            movement.y += 1.;
-        }
-
-        if keyboard_input.pressed(KeyCode::Down) {
-            movement.y -= 1.;
-        }
-
-        if movement != Vec3::new(0., 0., 0.) {
-            transform.translation += movement.normalize() * speed * time.delta_seconds();
-        }
+    if keyboard_input.pressed(KeyCode::Right) {
+        movement.x += 1.;
     }
+
+    if keyboard_input.pressed(KeyCode::Left) {
+        movement.x -= 1.;
+    }
+
+    if keyboard_input.pressed(KeyCode::Up) {
+        movement.y += 1.;
+    }
+
+    if keyboard_input.pressed(KeyCode::Down) {
+        movement.y -= 1.;
+    }
+
+    if movement != Vec3::new(0., 0., 0.) {
+        game.player.translation += movement.normalize() * speed * time.delta_seconds();
+    }
+
+    *transforms.get_mut(game.player.entity.unwrap()).unwrap() = Transform {
+        translation: game.player.translation,
+        ..default()
+    };
 }
